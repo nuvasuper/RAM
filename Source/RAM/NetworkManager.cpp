e07c2d08224869ebff6becb083527db690a6337d
nuvasuper <cdkrusemark@gmail.com>
2015-06-17 10:47:37 -0700

    Initial Commit

---
+++ Source/RAM/NetworkManager.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RAM.h"
+#include "NetworkManager.h"
+
+NetworkManager::NetworkManager()
+{
+}
+
+NetworkManager::~NetworkManager()
+{
+}
---
+++ Source/RAM/NetworkManager.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class RAM_API NetworkManager
+{
+public:
+	NetworkManager();
+	~NetworkManager();
+};
